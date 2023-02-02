ce488fb1864f4f7d5c5b36af99110386a5c8a15f
AveEst <122276459+AveEst@users.noreply.github.com>
2023-02-02 11:33:49 +0200

    uuendus

--- loeng07/arvavarv.js
+++ loeng07/arvavarv.js
@@ -2,5 +2,4 @@ let varv=prompt("Arva ära minu lemmikvärv")
 while (varv!=="roheline"){
     varv=prompt("Vale! arva uuesti, mis on minu lemmikvärv")
 }
-//19:15
-document.getelementbyid("valjund").innerhtml ="<h2>Jah - minu lemmikvärv on roheline"</h2>
+document.getElementById('valjund').innerHTML = '<h2>Jah - minu lemmikvärv on roheline</h2>'
