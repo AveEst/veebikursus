a434f632fd49d533b154f01186373fbe94e070c5
AveEst <122276459+AveEst@users.noreply.github.com>
2023-02-08 21:16:08 +0200

    uuendus

--- loeng06/nimi.js
+++ loeng06/nimi.js
@@ -2,4 +2,4 @@ let nimi
 nimi=prompt("Sisesta nimi")
 let tervitus=" Tere " + nimi
 alert(tervitus)
-document.getElementById("teade").innerHTML=tervitus
+document.getElementById("teade").innerHTML=teade
