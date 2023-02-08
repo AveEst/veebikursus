51ae50a98b7c4c4fa2f4739ab03b63e1225761d8
AveEst <122276459+AveEst@users.noreply.github.com>
2023-02-08 20:59:24 +0200

    uuendused

--- loeng10/tehavaja.js
+++ loeng10/tehavaja.js
@@ -1,58 +1,84 @@
-//järgnevad standardsed ülesanded on vaja teha igal hommikul
-
-const ylesanne1={
-    kirjeldus: "Lugeda head raamatut",
+//Järgnevad standardsed ülesanded mida on vaja on teha igal hommikul
+const ylesanne1 = {
+    kirjeldus: 'Lugeda head raamatut',
     olulisus: 3,
     kasTehtud: false,
-    kategooria: "heaolu"
+    kategooria: 'heaolu'
 }
-
-const ylesanne2={
-    kirjeldus: "Käia teatris või kinos",
+const ylesanne2 = {
+    kirjeldus: 'Käia teatris või kinos',
     olulisus: 3,
     kasTehtud: true,
-    kategooria: "heaolu"
+    kategooria: 'heaolu'
 }
-
-const ylesanne3={
-    kirjeldus: "Jookse kolm korda nädalas 3 kilomeetrit",
+const ylesanne3 = {
+    kirjeldus: 'Jookse kolm korda nädalas 3 kilomeetrit',
     olulisus: 4,
-    kasTehtud: false,
-    kategooria: "heaolu"
+    kasTehtud: true,
+    kategooria: 'heaolu'
 }
 
-const ylesanded=[
+const ylesanded = [
     ylesanne1,
     ylesanne2,
     ylesanne3
 ]
 
-//parameeter - objekt ülesanne
-//tagastab - teksti kujul ühe ülesande html-i
-function koostaYlesandeHTML(ylesanne){
-    let margitud=""
-
-if(ylesanne.kasTehtud){
-    margitud="checked"
+function muudaKasTehtudVaartusVastupidiseks(index) {
+    const ylesanne = ylesanded[index]
+    ylesanne.kasTehtud = !ylesanne.kasTehtud
+    valjastaYlesanded()
 }
 
-const ylesanneHTML=`
-<div class="ylesanne row">
-    <div class="kasTehtud col-1"><input type="checkbox" name="kasTehtud"${margitud}></div>
-    <div class="kirjeldus col-6">${ylesanne.kirjeldus}</div>
-    <div class="kategooria col-3">${ylesanne.kategooria}</div>
-    <div class="olulisus col-2">${ylesanne.olulisus}</div>
-</div>
-`
-return ylesanneHTML
+// parameeter  - objekt ylesanne
+// tagastab - teksti kujul ühe ülesande html-i
+function koostaYlesandeHTML(ylesanne, index) {
+    let margitud = ''
+    let tehtud = ''
+
+    if (ylesanne.kasTehtud) {
+        margitud = 'checked'
+        tehtud = 'tehtud'
+    }
+    
+    const ylesanneHtml = `
+    <div class="ylesanne row ${tehtud}">
+        <div class="kasTehtud col-1"><input onclick="muudaKasTehtudVaartusVastupidiseks(${index})" type="checkbox" name="kasTehtud" ${margitud} ></div>
+        <div class="kirjeldus col-6">${ylesanne.kirjeldus}</div>
+        <div class="kategooria col-3">${ylesanne.kategooria}</div>
+        <div class="oluisus col-2">${ylesanne.olulisus}</div>
+    </div>
+    `
+    return ylesanneHtml
 }
 
-let koguValjundHtml=""
+function valjastaYlesanded() {
+    let koguValjundHtml = ''
 
-for (let ylesanne of ylesanded){
-    koguValjundHtml+=koostaYlesandeHTML(ylesanne)
+    let index = 0
+    for (let ylesanne of ylesanded) {
+        koguValjundHtml += koostaYlesandeHTML(ylesanne, index)
+        index++
+    }
+    
+    document.getElementById('valjund').innerHTML =  koguValjundHtml
+}
+
+
+function lisaUusYlesanne() {
+    const ylesandeTekst = document.getElementById('ylesanne').value
+    const olulisus = document.getElementById('olulisus').value
+    const ylesanne = {
+        kirjeldus: ylesandeTekst,
+        olulisus: olulisus,
+        kasTehtud: false,
+        kategooria: 'heaolu'
+    }
+
+    ylesanded.push(ylesanne)
+    valjastaYlesanded()
 
 }
 
-//vajalik esmase väljundi saamiseks
-document.getElementById("valjund").innerHTML=koguValjundHtml
+ //vajalik esmase väljund joonistamiseks
+ valjastaYlesanded()
